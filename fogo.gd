111bef43be359836964f45690f60eed98018c86e
VictorLScheuerRodrigues <victor.scheuer.rodrigues@gmail.com>
2025-10-24 21:35:42 -0300

    build
    add fogos jogador bombeiro com configs de script

--- fogo.gd
+++ fogo.gd
@@ -1,4 +1,9 @@
 extends StaticBody2D
 
 func _ready():
-	$AnimatedSprite2D.play()
+	add_to_group("fogo") # Grupo pra identificar o tipo do objeto
+	$AnimatedSprite2D.play() # Toca a animação do fogo
+
+func extinguish():
+	$AnimatedSprite2D.stop()
+	queue_free() # remove o fogo da cena (apagado)
