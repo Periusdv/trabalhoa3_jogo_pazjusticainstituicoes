38896f03e66aa65163286090c52fcf23f2906007
Periusdv <dvperius@gmail.com>
2025-10-30 16:52:21 -0300

    build:
    Background está se movimentando conforme o personagem anda.

--- back_grond.gd
+++ back_grond.gd
@@ -1,8 +1,19 @@
 extends Node2D
 
-@export var scroll_speed := 0.5
+@export var velocidade_fundo : float = 0.04  # ajuste conforme quiser
+var shader_material : ShaderMaterial
 
-func _process(_delta):
-	var player = get_node("../player") # ajusta o caminho do player
-	if player:
-		$TextureRect.material.set_shader_parameter("player_position", player.global_position / 1000.0)
+func _ready():
+	# Assumindo que o TextureRect é filho direto
+	shader_material = $TextureRect.material as ShaderMaterial
+
+func _process(delta):
+	# Captura input horizontal do personagem
+	var input = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	
+	if input != 0:
+		# Atualiza o offset do shader, movendo da direita para esquerda
+		shader_material.set_shader_parameter(
+			"offset_x",
+			shader_material.get_shader_parameter("offset_x") + input * velocidade_fundo * delta
+		)
