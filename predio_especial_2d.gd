d40c525c20fbc8ad5d3a717ce867031cf46a0eb4
Periusdv <dvperius@gmail.com>
2025-11-04 19:15:25 -0300

    build:
    Criação da tela de fim de jogo e funcionado com o jogador

--- predio_especial_2d.gd
+++ predio_especial_2d.gd
@@ -7,4 +7,4 @@ func _ready():
 func _on_body_entered(body):
 	if body.is_in_group("player"):
 		print("Fim de jogo!")
-		get_tree().SceneTree("res://gameOver.tscn")  # substitua pelo caminho da tela de fim
+		get_tree().change_scene_to_file("res://tela_fim_de_jogo.tscn")  # substitua pelo caminho da tela de fim
